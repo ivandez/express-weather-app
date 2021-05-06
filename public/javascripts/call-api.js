0c71bd08b137b165e2056e75550f5c9316d31930
Ivan Mendez <ivanmr932@gmail.com>
2021-05-06 19:40:43 -0400

    add weatherstack api call logic

--- public/javascripts/call-api.js
+++ public/javascripts/call-api.js
@@ -1,39 +1,39 @@
-const country = document.querySelector("#country");
-const name = document.querySelector("#name");
-const temperature = document.querySelector("#temperature");
-const form = document.querySelector("#weather-form");
-const userInput = document.querySelector("#userInput");
-const spinner = document.getElementById("spinner");
+const country = document.querySelector('#country')
+const name = document.querySelector('#name')
+const temperature = document.querySelector('#temperature')
+const form = document.querySelector('#weather-form')
+const userInput = document.querySelector('#userInput')
+const spinner = document.getElementById('spinner')
 
-const showSpinner = () => {
-  spinner.className = "show";
-  setTimeout(() => {
-    spinner.className = spinner.className.replace("show", "");
-  }, 5000);
-};
+// const showSpinner = () => {
+//   spinner.className = 'show'
+//   setTimeout(() => {
+//     spinner.className = spinner.className.replace('show', '')
+//   }, 5000)
+// }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-  spinner.className = "show";
-  fetch(`http://localhost:3000/get-weather/country/${userInput.value}`).then(
+  spinner.className = 'show'
+  fetch(`http://localhost:3000/weather/get/${userInput.value}`).then(
     (res) => {
       res.json().then((data) => {
-        spinner.className = spinner.className.replace("show", "");
+        spinner.className = spinner.className.replace('show', '')
 
         if (data.error) {
-          console.log("error");
+          console.log('error')
           // console.log(data.error)
         } else {
-          let temperatureF = (data.temperature * 9) / 5 + 32;
-          country.textContent = data.location;
-          name.textContent = data.name;
-          temperature.textContent = `${data.temperature}°C/${temperatureF}°F`;
+          const temperatureF = (data.temperature * 9) / 5 + 32
+          country.textContent = data.location
+          name.textContent = data.name
+          temperature.textContent = `${data.temperature}°C/${temperatureF}°F`
         }
-      });
+      })
     }
-  );
-});
+  )
+})
 
 // const showSpinner = () => {
 //   spinner.className = "show";
--- routes/weather.js
+++ routes/weather.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const router = express.Router()
-
+const axios = require('axios')
 /* GET WEATHER */
 
 router.get('/get/:country', (req, res, next) => {
-  res.send(req.params.country)
+  const COUNTRY = encodeURI(req.params.country)
+
+  axios.get(`http://api.weatherstack.com/current?access_key=fbca4f6a360e658244c5ac5a7f9bda21&query=${COUNTRY}`)
+
+    .then(function (response) {
+      // handle success
+      if (response.data.error) {
+        return res.send('Hubo un error, por favor intentalo mas tarde')
+      }
+      const weather = {
+        name: response.data.location.name,
+        location: response.data.location.country,
+        temperature: response.data.current.temperature
+      }
+      res.send(weather)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+    })
+
+  // const weather = {
+  //   name: 'asd',
+  //   location: 'asd',
+  //   temperature: 33
+  // }
+
+  // setTimeout(() => {
+  //   res.send(weather)
+  // }, 2000)
 })
 
 module.exports = router
