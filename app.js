a86ab991930416a4f4426eb6270a24654012c811
Ivan Mendez <ivanmr932@gmail.com>
2021-05-06 16:13:56 -0400

    set app settings

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
-
 const app = express()
 
+// APP SETTINGS
+const PORT = 3000
+const publicPath = path.join(__dirname, '/public')
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'html')
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+//  ROUTES
+app.get('/', (req, res) => {
+  res.sendFile('inicio', { root: publicPath })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server up listen ${PORT} port`)
+})
 
 module.exports = app
